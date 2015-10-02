cd0e31b5baf19df2efe633946b37a1894bc4c945
unknown <dmitriyg@ptua-ls0378.EE.playtech.corp>
2015-10-02 22:11:11 +0300

    Homework 1

---
+++ traininig/src/com/artcode/rent/dao/RentalRequirementsDao.java
@@ -0,0 +1,7 @@
+package com.artcode.rent.dao;
+
+/**
+ * Created by dmitriyg on 9/25/2015.
+ */
+public interface RentalRequirementsDao {
+}
---
+++ traininig/src/com/artcode/rent/model/User.java
@@ -0,0 +1,7 @@
+package com.artcode.rent.model;
+
+/**
+ * Created by dmitriyg on 9/25/2015.
+ */
+public class User {
+}
