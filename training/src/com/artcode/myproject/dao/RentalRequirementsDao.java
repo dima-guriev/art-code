666f6aabc8a90d9e57c57a22cadf2ef76727bed6
dima-guriev <dima.guriev12@gmail.com>
2015-10-15 20:15:58 +0300

    switched to maven

--- training/src/com/artcode/myproject/dao/RentalRequirementsDao.java
+++
@@ -1,7 +0,0 @@
-package com.artcode.myproject.dao;
-
-/**
- * Created by dmitriyg on 9/25/2015.
- */
-public interface RentalRequirementsDao {
-}
--- training/src/com/artcode/myproject/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.artcode.myproject.service;
-
-/**
- * Created by dmitriyg on 9/25/2015.
- */
-public interface UserService {
-}
